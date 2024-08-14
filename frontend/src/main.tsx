ee37bb9825678ab9d3c063a62e655909de50a257
prayag-raj <prayag07.mathuria@gmail.com>
2024-08-14 16:13:59 +0530

    Add initial frontend of the App
    Structured the intial Project, added required components directories, like api, assets, auth0, components, pages, and other main files like AppRoutes.tsx, global.css, main.tss, index.html, json-files, and other required files

---
+++ frontend/src/main.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import "./global.css";
+import Auth0ProviderWithNavigate from "./auth0/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
+    </Router>
+  </React.StrictMode>
+);
