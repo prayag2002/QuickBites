dfcd739ae314715b4d6d28ae60ddd468c5844dd7
prayag-raj <prayag07.mathuria@gmail.com>
2024-08-14 16:18:52 +0530

    Add the intial backend of the App
    Structured the backend, structured src, eg. controllers, models, routes and other files

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import MyUserRoute from "./routes/MyUserRoute";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to Database Successfully!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/my/user", MyUserRoute);
+
+app.listen(7000, () => {
+  console.log("server started successfully on localhost:7000");
+});
