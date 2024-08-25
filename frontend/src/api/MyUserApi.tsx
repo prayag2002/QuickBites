4f9eacf5d6da958bac0736d8f922344467dc2980
prayag-raj <prayag07.mathuria@gmail.com>
2024-08-25 20:45:21 +0530

    added feature toast-notification

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; //importing the base url from the .env file
 
@@ -80,10 +81,18 @@ export const useUpdateMyUser = () => {
     mutateAsync: updateUser,
     isLoading,
     isSuccess,
-    isError,
     error,
     reset,
   } = useMutation(updateMyUserRequest);
+
+  if (isSuccess) {
+    toast.success("User profile updated successfully");
+  }
+
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
 
   return {
     updateUser,
