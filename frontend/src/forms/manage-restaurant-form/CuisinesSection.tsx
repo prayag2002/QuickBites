84919132db7ec3b93ce2079523112c5fc20ba2b7
prayag-raj <prayag07.mathuria@gmail.com>
2024-08-29 23:18:11 +0530

    Added feature Restaurant Form
    created manage restaurant page, created restaurat manage form, created detail, cuisine, menuItem, and Image Section

---
+++ frontend/src/forms/manage-restaurant-form/CuisinesSection.tsx
@@ -0,0 +1,40 @@
+import {
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { cuisineList } from "@/config/restaurant-options-config";
+import { useFormContext } from "react-hook-form";
+import CuisineCheckbox from "./CuisineCheckbox";
+
+const CuisinesSection = () => {
+  const { control } = useFormContext(); //useFormContext is a custom hook that returns the context value for the form. This is useful when you have a deeply nested form and you want to access the context values at a deeper level.
+
+  return (
+    <div className="space-y-2">
+      <div>
+        <h2 className="text-2xl font-bold">Cuisines</h2>
+        <FormDescription>
+          Select the cuisines that your restaurant serves
+        </FormDescription>
+      </div>
+      <FormField
+        control={control}
+        name="cuisines"
+        render={({ field }) => (
+          <FormItem>
+            <div className="grid md:grid-cols-5 gap-1">
+              {cuisineList.map((cuisineItem) => (
+                <CuisineCheckbox cuisine={cuisineItem} field={field} />
+              ))}
+            </div>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+};
+
+export default CuisinesSection;
